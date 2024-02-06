d4c82ea05c42cdaf2f255a6b7c29d88d7147b786
Harshvardhan <shreeharshpb11@gmail.com>
2024-02-06 23:44:37 +0530

    create signup api

---
+++ api/controller/auth.controller.js
@@ -0,0 +1,19 @@
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs'
+
+export const signup = async (req, res) => {
+    const { userName, email, password } = req.body;
+    const hashedPassword =  await bcryptjs.hash(password, 10);
+    const newUser = new User({ userName, email, password : hashedPassword });
+
+    try {
+
+        await newUser.save()
+        res.status(200).json({
+            'Message': 'User Created Successfully'
+        })
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
+
