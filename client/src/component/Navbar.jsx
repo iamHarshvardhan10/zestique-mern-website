fba3b892336b599bf87ba1b0b1d7160d5fca10af
Harshvardhan <shreeharshpb11@gmail.com>
2024-02-06 16:49:09 +0530

    create navbar page

---
+++ client/src/component/Navbar.jsx
@@ -0,0 +1,27 @@
+import { BsCart4 } from "react-icons/bs";
+import {Link} from 'react-router-dom'
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="navbarLogo">
+        <Link to="/">
+        <h2>Zestique</h2>
+        </Link>
+      </div>
+      <div className="navList">
+        <ul>
+          <li>Menu</li>
+          <li>Reserve A Table</li>
+          <Link to="/sign-in">
+          <li>Sign In</li>
+          </Link>
+          <li>
+            <BsCart4 />
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
