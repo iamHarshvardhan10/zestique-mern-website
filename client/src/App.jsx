9f446af7a3102c58f4f45eeac4a9fada0c3f7b1c
Harshvardhan <shreeharshpb11@gmail.com>
2024-02-27 00:24:58 +0530

    update in app component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,21 @@ import Home from "./component/Home";
 import Navbar from "./component/Navbar";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Profile from "./component/Profile";
+import PrivatePage from "./pages/PrivatePage";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-      <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/sign-in" element={<SignIn/>}/>
-          <Route path="/sign-up" element={<SignUp/>}/>
-
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route element={<PrivatePage/>}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
