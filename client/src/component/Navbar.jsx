9b314d83ef29902a771f15a235c9106c9811d4c6
Harshvardhan <shreeharshpb11@gmail.com>
2024-02-27 00:21:20 +0530

    Complete Navbar Page design Functionality

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,20 +1,34 @@
 import { BsCart4 } from "react-icons/bs";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 const Navbar = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <nav>
       <div className="navbarLogo">
         <Link to="/">
-        <h2>Zestique</h2>
+          <h2>Zestique</h2>
         </Link>
       </div>
       <div className="navList">
         <ul>
           <li>Menu</li>
           <li>Reserve A Table</li>
-          <Link to="/sign-in">
-          <li>Sign In</li>
-          </Link>
+
+          {currentUser ? (
+            
+              <Link to="/profile">
+                <p style={{ color: "white" }}>
+                  Hello, {`${currentUser.userName}`}
+                </p>
+            
+                </Link>
+          ) : (
+            <Link to="/sign-in">
+              <li>Sign In</li>
+            </Link>
+          )}
+
           <li>
             <BsCart4 />
           </li>
