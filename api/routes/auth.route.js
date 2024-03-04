1920f784dea8ee981e339dc81239a34873be2874
Harshvardhan <shreeharshpb11@gmail.com>
2024-03-04 21:12:35 +0530

    create auth route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { signup } from '../controller/auth.controller.js'
+import { signup , signIn, signOut } from '../controller/auth.controller.js'
 
 
 const router = express.Router()
 
 router.post('/signup' , signup)
-
+router.post('/signIn' , signIn)
+router.get('/signOut' , signOut)
 
 export default router;
