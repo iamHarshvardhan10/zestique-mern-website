20c870ac251c6f59f503a115ca7454583dad8ba1
Harshvardhan <shreeharshpb11@gmail.com>
2024-03-04 21:11:50 +0530

    created authentication route functionality

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,10 +1,11 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res) => {
     const { userName, email, password } = req.body;
-    const hashedPassword =  await bcryptjs.hash(password, 10);
-    const newUser = new User({ userName, email, password : hashedPassword });
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const newUser = new User({ userName, email, password: hashedPassword });
 
     try {
 
@@ -17,3 +18,39 @@ export const signup = async (req, res) => {
     }
 }
 
+
+
+export const signIn = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const validedUser = await User.findOne({ email })
+        if (!validedUser) {
+            res.send({ 'message': 'User Not Found' })
+        }
+        const validedPassword = bcryptjs.compareSync(password, validedUser.password)
+
+        if (!validedPassword) {
+            res.send({ 'Message': 'Wrong Credentails' })
+        }
+
+        const token = jwt.sign({ id: validedUser._id }, process.env.JWT_SECRET)
+        const {password : pass , ...rest} = validedUser._doc;
+
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest)
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+
+export const signOut = (req , res , next) => {
+    try {
+        res.clearCookie('access_token')
+        res.status(204).json({ message:'Sign Out Successfuly'})
+    } catch (error) {
+        next(error)
+    }
+}
