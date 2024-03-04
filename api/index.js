6e9397e4a321a7d8637a58d1ca29716ccfd278ee
Harshvardhan <shreeharshpb11@gmail.com>
2024-03-04 21:11:29 +0530

    created server and routes

--- api/index.js
+++ api/index.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
+
+
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv'
 
 // PATH OF API ROUTE  FILES :- TEST API
 
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
+import listingRouter from './routes/listing.route.js'
 
 dotenv.config()
 
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log('Successfully connected to MongoDB')
 }).catch((error) => {
     console.log(error)
@@ -18,15 +23,25 @@ const app = express()
 
 const Port = 3000
 
+app.use(express.json())
+app.use(cookieParser())
 
 // API CALLING FROM OTHER FOLDER
 
 // SIMPLE GET TEST API 
- 
-app.use('/api/user' , userRouter)
+
+app.use('/api/user', userRouter)
+
+// SIGNUP POST API
+
+app.use('/api/auth', authRouter)
+
+
+// CREATE THE MENU LISTING
+app.use('/api/listing', listingRouter)
 
 // created Server which is running on port number 3000
 
 app.listen(Port, () => {
     console.log(`Server is Running on  ${Port}`)
-})
+})  
