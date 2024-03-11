6611be1625073144e59a95ce1a9484a7c639e66e
Harshvardhan <shreeharshpb11@gmail.com>
2024-03-11 23:49:52 +0530

    Create Firebase Configuration for image Uploading

---
+++ client/src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZONv6r69dzWyV4KpLbaY-xb3UxSRLM3s",
+  authDomain: "foodapp-d7b60.firebaseapp.com",
+  projectId: "foodapp-d7b60",
+  storageBucket: "foodapp-d7b60.appspot.com",
+  messagingSenderId: "747261876359",
+  appId: "1:747261876359:web:36d78d622080f1ea6dc022"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
